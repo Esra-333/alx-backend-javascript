1bc48c0755e84dd700f60f217022328371e009bd
Esra-333 <esra.omer444@gmail.com>
2024-07-25 02:28:50 -0700

    a

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -4,7 +4,7 @@ export default class ClassRoom {
    * @param {Number} maxStudentsSize - The maximum number of
    * students in the class.
    */
-constructor(maxStudentsSize) {
-  this._maxStudentsSize = maxStudentsSize;
-}
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
 }
